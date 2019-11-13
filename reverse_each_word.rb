9d6f1b783ace01926fa24e76dcf776757706eeb7
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 02:02:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array_of_string  = string.split (" ")
   
-  array_of_string.collect{|i| i.reverse}
+  array_of_string.collect{|i| i.reverse!}
     
   pp array_of_string
   
