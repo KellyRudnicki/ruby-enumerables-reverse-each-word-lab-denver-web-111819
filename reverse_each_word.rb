8e97d2dfe517ced9a0c7fdae94ebe88633d494a9
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 02:10:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ def reverse_each_word(string)
   
   array_of_string.collect{|i| i.reverse!}
   
-  new_array = array_of_string
+  new_array = array_of_string.flatten.join(" ")
+  
   pp array_of_string
   
 end 
